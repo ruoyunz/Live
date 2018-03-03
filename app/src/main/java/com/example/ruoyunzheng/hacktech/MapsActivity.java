af2f183a8492096ed53fd26aea3266a462e21db4
Ruoyun <ruoyunzheng@gmail.com>
2018-03-03 03:19:28 -0800

    Set map type.

--- app/src/main/java/com/example/ruoyunzheng/hacktech/MapsActivity.java
+++ app/src/main/java/com/example/ruoyunzheng/hacktech/MapsActivity.java
@@ -39,7 +39,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
